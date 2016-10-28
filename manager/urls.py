514728c9e3dbb79488243d5989f37b0baa291f1d
kirankonduru <kiranckonduru@gmail.com>
2016-10-29 02:29:13 +0530

    manager reviewers

--- manager/urls.py
+++ manager/urls.py
@@ -3,5 +3,6 @@ from manager import views
 
 urlpatterns = [
 	url(r'^$', views.home),
-	url(r'^assignreviewer/', views.assign_reviewer),
+	url(r'^assignreviewer/(?P<paper_id>[A-Za-z0-9.-]+)/$', views.assign_reviewer),
+	url(r'^reviewcomplete/(?P<paper_id>[A-Za-z0-9.-]+)/(?P<u_id>[A-Za-z0-9.-]+)/$', views.reviewCompleted),
 ]
