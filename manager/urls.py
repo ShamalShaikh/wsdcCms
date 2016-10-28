b572737295052ec27a188a151e67a46421c1747d
priyamkhandelwal <priyamkhandelwalbtp@gmail.com>
2016-10-29 00:27:02 +0530

    manager login

--- manager/urls.py
+++ manager/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import url,include
 from manager import views
 
 urlpatterns = [
-	
+	url(r'^$',views.home),
+	url(r'^signin/$',views.signin,name='signin'),
+	url(r'^signin_auth/$',views.signin_auth,name='signin_auth'),
+
 ]
--- manager/views.py
+++ manager/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.shortcuts import render, render_to_response, redirect
+from django.http import HttpResponse, HttpResponseRedirect, HttpRequest, Http404,JsonResponse
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required,user_passes_test
+from django.contrib.auth.models import User
+from conference.models import Conference
+from manager.models import *
 
-# Create your views here.
+def signin(request):
+	return render(request,'login_auth/sites/manager_signin.djt')
+
+def signin_auth(request):
+	reponse={}
+	username = request.POST['username']
+	user = User.objects.get(username=username)
+	manager = Manager.objects.get(user=user)
+	if request.user.is_authenticated() and manager:
+	    return redirect('/manager')
+	if request.method == "POST" and manager:
+	    password = request.POST['password']
+	    user = authenticate(username=username, password=password)
+	    if user is not None:
+	        login(request, user)
+	        return redirect('/manager')
+	    else:
+	        response['message']='User is not registered/Password Incorrect' 
+	return render(request,'login_auth/sites/manager_signin.djt',response)
+
+def home(request):
+	return
