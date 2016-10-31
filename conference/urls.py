a94ea04fdf9c0c2624e20d3ac5a7ae35a159f0c6
priyamkhandelwal <priyamkhandelwalbtp@gmail.com>
2016-10-31 22:34:48 +0530

    minor change made

--- conference/urls.py
+++ conference/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
 	url(r'^$',views.index),
 	url(r'^make_payment/$',views.make_payment,name='make_payment'),
 	url(r'^payment/$',views.payment,name='payment'),
+	url(r'^upload_paper/(?P<cid>[0-9]+)/$',views.upload_paper,name='upload_paper'),
 ]
