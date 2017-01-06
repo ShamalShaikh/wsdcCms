b500b7293ce057b557165a9f1cc198fcf96674a9
Vivek Unnikrishnan <acemaster@Viveks-MacBook-Pro.local>
2017-01-07 00:25:51 +0530

    Completed urls

--- login_auth/urls.py
+++ login_auth/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import url,include
+from django.views.generic import RedirectView
 from . import views
 
 urlpatterns = [
-	url(r'^$',views.index),
+	url(r'^$',RedirectView.as_view(url='/conference/treat17/')),
 	url(r'^Register/$',views.Register,name='Register'),
 	url(r'^register/$',views.register,name='register'),
 	url(r'^signin/$',views.signin,name='signin'),
 	url(r'^signout/$',views.signout,name='signout'),
-	url(r'^dashboard/$',views.dashboard,name='dashboard'),
+	url(r'^dashboard/$',RedirectView.as_view(url='/conference/treat17/')),
 	url(r'^profile/(?P<type>[0-9])/$', views.profile, name='profile'),
 ]
