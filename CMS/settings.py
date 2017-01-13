4ad110d754d529a809ade6ab4b18920d939e579b
Vivek Unnikrishnan <acemaster@Viveks-MacBook-Pro.local>
2017-01-13 12:26:11 +0530

    Completed ga,url fixes, and apache conf

--- CMS/settings.py
+++ CMS/settings.py
@@ -84,7 +84,8 @@ WSGI_APPLICATION = 'CMS.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
+#For Production Uncomment the below line
+# from databases import DATABASES
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
