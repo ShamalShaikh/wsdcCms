2ebcf0f0d859d643aec22fd01bf6630161468123
Tauhid <saitejach127@gmail.com>
2019-11-22 18:41:23 +0530

    push

--- CMS/databases.py
+++ CMS/databases.py
@@ -3,7 +3,7 @@ DATABASES = {
         'ENGINE':'django.db.backends.mysql',
         'NAME': 'treat17',
         'USER': 'root',
-        'PASSWORD': 'mysql_password',
+        'PASSWORD': 'wsdcadmin',
         'HOST': 'localhost', 
     }
 }
